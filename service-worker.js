67539a3cd1b1da461ac6dc9be5e2b32483a9ea81
NFD-Joel <xaytag@gmail.com>
2026-02-17 16:43:45 -0300

    Update service-worker.js
    update

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,10 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("nfd-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json"
+      ]);
+    })
+  );
 });
